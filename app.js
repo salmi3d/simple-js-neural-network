b9b1ebdcfb7ef98ce57d12b500afa55dc1fcbd65
salmi3d <salmi3d@gmail.com>
2018-07-28 11:11:53 +0300

    draft

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
 (function(){
-  alert('yo')
+
+  class App {
+
+    constructor(el) {
+      alert(el)
+      this.setHandlers()
+    }
+
+    setHandlers() {
+      document.addEventListener('click', e => {
+
+        if (!e.target) {
+          return
+        }
+
+        const id = e.target.id
+
+        if(id === 'clear') {
+          this.clear()
+        }
+
+        if(id === 'train') {
+          this.train()
+        }
+
+        if(id === 'guess') {
+          this.guess()
+        }
+
+      })
+    }
+
+    clear() {
+      alert('clear')
+    }
+
+    train() {
+      alert('train')
+    }
+
+    guess() {
+      alert('guess')
+    }
+
+  }
+
+  new App(document.getElementById('canvas'))
+
 })()
