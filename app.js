2dfd7b5ea9b70ad4f4fe6d3441dc038504b6f405
salmi3d <salmi3d@gmail.com>
2018-08-04 11:52:49 +0300

    added draw ability

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
   class App {
 
     constructor(el) {
-      alert(el)
+      this.canvas = el
+      this.isDraw = false
+      this.spot = 24;
+      this.ctx = this.canvas.getContext('2d')
       this.setHandlers()
     }
 
@@ -29,6 +32,35 @@
         }
 
       })
+
+      this.canvas.addEventListener('mousedown', () => {
+				this.isDraw = true
+				this.ctx.beginPath()
+			})
+
+			this.canvas.addEventListener('mouseup', () => {
+				this.isDraw = false
+			})
+
+			this.canvas.addEventListener('mousemove', e => {
+				if(!this.isDraw) {
+          return
+        }
+        this.ctx.lineWidth = this.spot
+        this.ctx.strokeStyle = 'red'
+        this.ctx.fillStyle = 'red'
+
+        this.ctx.lineTo(e.offsetX, e.offsetY)
+        this.ctx.stroke()
+
+        this.ctx.beginPath();
+        this.ctx.arc(e.offsetX, e.offsetY, this.spot / 2, 0, Math.PI * 2)
+        this.ctx.fill()
+
+        this.ctx.beginPath()
+        this.ctx.moveTo(e.offsetX, e.offsetY)
+      })
+
     }
 
     clear() {
